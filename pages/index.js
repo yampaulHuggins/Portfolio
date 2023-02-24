aa40549701e3a25ddc5b60f03ae6da1cb4504fd0
Yampaul <yhuggins@Insolpr.com>
2023-02-24 14:51:01 -0400

    Fixing images issue

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,11 @@ import Head from 'next/head';
 import {BsFillMoonStarsFill} from 'react-icons/bs';
 import {AiFillLinkedin} from 'react-icons/ai';
 import Image from 'next/image';
-import yampaul from '../public/yampaul.jpg';
-import design from '../public/design.png';
-import code from '../public/code.png';
-import consulting from '../public/consulting.png';
-import web1 from '../public/web1.png';
-import web2 from '../public/web2.png';
-import web3 from '../public/web3.png';
-import web4 from '../public/web4.png';
-import web5 from '../public/web5.png';
-import web6 from '../public/web6.png';
-import ff360 from '../public/ff360.png';
+import yampaul from './../public/yampaul.jpg'
+import design from './../public/design.png';
+import code from './../public/code.png';
+import consulting from './../public/consulting.png';
+import ff360 from './../public/ff360.png';
 import {useState} from 'react';
 
 export default function Home() {
