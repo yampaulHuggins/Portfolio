79d610e33bec607da5b1945531a2920fe42dd65a
Yampaul <yhuggins@Insolpr.com>
2023-02-28 18:33:45 -0400

    fixing overall functionality, and look & feel

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "84bcf2ca414c8fdd"; }
+/******/ 		__webpack_require__.h = function() { return "dcd137e3ec83dedf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import {BsFillMoonStarsFill} from 'react-icons/bs';
-import {AiFillLinkedin} from 'react-icons/ai';
+import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
 import Image from 'next/image';
 import {useState} from 'react';
 
@@ -26,9 +26,9 @@ export default function Home() {
               <li>
                 <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl dark:text-gray-200"/>
               </li>
-              <li>
+              {/* <li>
                 <a className="bg-gradient-to-r from-cyan-500 to-teal-500  text-white px-4 py-2 rounded-md ml-8" href="#">Resume</a>
-              </li>
+              </li> */}
             </ul>
           </nav>
           <div className="text-center p-10">
@@ -37,17 +37,22 @@ export default function Home() {
             </h2>
             <h3 className="text-2xl py-2 md:text-3xl dark:text-gray-50">Web Developer/Software Engineer</h3>
             <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-gray-400">
-              Front-End Developer looking for oppotunities in similar jobs. 
+              Front-End Developer looking for oppotunities in similar jobs. Exploring new opportunities.
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
-              <AiFillLinkedin />
+              <a target="_blank" href='https://www.linkedin.com/in/yampaul-huggins-1858741bb/'>
+                <AiFillLinkedin />
+              </a>
+              <a target="_blank" href='https://github.com/yampaulHuggins'>
+                <AiFillGithub />
+              </a>
             </div>
-            <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
-              <Image loader={myLoader} src="yampaul.png" layout="fill" objectFit="cover"/>
+            <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full sm:w-full md:w-80 lg:w-80 h-80 mt-20 mb-10 overflow-hidden">
+              <Image loader={myLoader} alt="Picture of Yampaul" src="yampaul.png" layout="fill" objectFit="cover"/>
             </div>
             <section>
               <div>
-                <h3 className="text-3xl py-1 dark:text-gray-50">Services I offer</h3>
+                {/* <h3 className="text-3xl py-1 dark:text-gray-50">Experience</h3> */}
                 <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-500">
                   Since the beginning of my journey, I developed a passion towards user interfaces,
                   looks and feels. Trying to catch the user's attention and giving them the best experience overall,
@@ -59,8 +64,8 @@ export default function Home() {
                   <Image loader={myLoader} src="design.png" width={100} height={100}/>
                   <h3 className="text-lg font-medium pt-8 pb-2">Current Job</h3>
                   {/* <p className="py-2"></p> */}
-                  <h4 className="py-4 text-teal-600">What I've been working on:</h4>
-                  <ul className="list-disc">
+                  <h4 className="py-4 text-teal-600 dark:text-gray-900 underline">What I've been working on:</h4>
+                  <ul>
                     <li className="text-gray-800 py-1">Migrating API's to Springboot (Java)</li>
                     <li className="text-gray-800 py-1">Creating/Editing modules on Angular.js</li>
                     <li className="text-gray-800 py-1">Migrating modules from Angular.js to Angular 12</li>
@@ -70,8 +75,8 @@ export default function Home() {
                   <Image loader={myLoader} src="code.png" width={100} height={100}/>
                   <h3 className="text-lg font-medium pt-8 pb-2">Freelancing</h3>
                    {/* <p className="py-2"></p> */}
-                  <h4 className="py-4 text-teal-600">What I've been working on:</h4>
-                  <ul className="list-disc">
+                  <h4 className="py-4 text-teal-600 dark:text-gray-900 underline">What I've been working on:</h4>
+                  <ul>
                     <li className="text-gray-800 py-1">Creating Single page applications with React</li>
                     <li className="text-gray-800 py-1">Creating Node.js projects</li>
                     <li className="text-gray-800 py-1">Publishing Websites</li>
@@ -81,8 +86,8 @@ export default function Home() {
                   <Image loader={myLoader} src="consulting.png" width={100} height={100}/>
                   <h3 className="text-lg font-medium pt-8 pb-2">What's next?</h3>
                   {/* <p className="py-2"></p> */}
-                  <h4 className="py-4 text-teal-600">What I aspire to everyday:</h4>
-                  <ul className="list-disc">
+                  <h4 className="py-4 text-teal-600 dark:text-gray-900 underline">What I aspire to everyday:</h4>
+                  <ul>
                     <li className="text-gray-800 py-1">Becoming a better person</li>
                     <li className="text-gray-800 py-1">Becoming a better Programmer/Web Developer</li>
                     <li className="text-gray-800 py-1">Learning from experiences</li>
@@ -93,22 +98,31 @@ export default function Home() {
             </section>
             <section>
               <div>
-                <h3 className="text-3xl py-1 dark:text-gray-50">My work</h3>
-                <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-500">
-                  Since the beginning of my journey, I developed a passion towards user interfaces,
-                  looks and feels. Trying to catch the user's attention and giving them the best experience overall,
-                  without sacrificing functionality. 
-                </p>
+                <div className=' rounded-xl dark:bg-gray-400'>
+                  <h3 className="text-3xl py-1">Projects</h3>
+                  {/* <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-500">
+                    Since the beginning of my journey, I developed a passion towards user interfaces,
+                    looks and feels. Trying to catch the user's attention and giving them the best experience overall,
+                    without sacrificing functionality. 
+                  </p> */}
+                </div>
                 <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-                  <div className="basis-1/3 flex-1">
-                    <Image loader={myLoader} src="ff360.png" className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive" />
-                  </div>
+                    <div className="basis-1/3 flex-1">
+                      <a href="https://ff360pr.com" target="_blank">
+                        <Image loader={myLoader} src="ff360.png" className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive" />
+                      </a>
+                    </div>
                 </div>
               </div>
             </section>
           </div>
         </section>
       </main>
+      {/* <footer>
+        <div>
+          <p>2023</p>
+        </div>
+      </footer> */}
     </div>
   )
 }
